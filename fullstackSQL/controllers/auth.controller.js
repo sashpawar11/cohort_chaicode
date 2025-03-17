6d47dba389600fc8a3549be8856ab369f7a07fd6
sashpawar11 <saisaipawar@gmail.com>
2025-03-17 10:24:26 +0530

    add login,register controllers

--- fullstackSQL/controllers/auth.controller.js
+++ fullstackSQL/controllers/auth.controller.js
@@ -1,27 +1,132 @@
-import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import bcrypt from 'bcryptjs'
+import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
 
+dotenv.config();
 
 const prisma = new PrismaClient();
 
 
 const registerUser = async (req, res) => {
 
-    await prisma.user.findUnique({
-     where : {email}
-    })
+    const { name, email, password, phone } = req.body
+    
+    if (!name || !email || !password || !phone) {
+        console.log("all fields not found")
+        res.status(400).json({
+            message: "All fields are mandatory!",
+            success : false
+        })
+    }
+    console.log("user register route")
+   try {
+     const user = await prisma.user.findUnique({
+         where: { email }
+     })
+ 
+     if (user) {
+        res.status(400).json({
+            message: "User already existing!",
+            success : false
+        })
+     }
+     
+    // hash the pass
+    const hashedpass = await bcrypt.hash(password, 10);
+    
+       //verfiication token
+       const verfiicationToken = crypto.randomBytes(32).toString("hex");
+
+       
+       
+       const newuser = await prisma.user.create({
+           data: { name: name, email: email, phone ,password: hashedpass, verificationToken}
+       });
+
+       sendEmailUtil(
+        newuser.email,
+        "Verify your email address!",
+        `Please click on the following link :
+                  ${process.env.BASE_URL}/api/v1/users/verify/${verfiicationToken}`,
+      );
+
+       
+   } catch (error) {
+        res.status(400).json({
+        message: `User not registered ! Err: ${error}`,
+        success: false,
+      });
+   }
+}
 
 
 
 const loginUser = async (req, res) => {
 
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.status(400).json({
+            message: "All fields are mandatory!",
+            success : false
+        })
+    }
+    
+    try {
+    
+        const findUser = user.findUnique({ where: email });
+        if (!findUser) {
+            res.status(400).json({
+                message: "User not found!",
+                success : false
+            })
+        }
+    
+        const isMatch = await bcrypt.compare(password, findUser.password);
+        if (!isMatch) {
+            return res.status(400).json({
+              message: "Invalid credentials!",
+            });
+        }
+        
+        // generate JWT Token
+        const jwtToken = jwt.sign({ id: findUser.id, role: findUser.role }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        
+        // set jwt token to user cookie using cookie-parser
+        const cookieOptions = {
+            httpOnly: true, // only accessible to backend
+            secure: true,
+            maxAge: 24 * 60 * 60 * 1000, // 24h
+          };
+      
+        res.cookie("jwtToken", jwtToken, cookieOptions);
+        
+        return res.status(200).json({
+            message: "Sucess, logged in!",
+            success: true,
+            user: {
+              id: userExists._id,
+              name: userExists.name,
+              role: userExists.role,
+            },
+            token: jsontoken,
+          });
+    } catch (error) {
+    return res.status(401).json({
+        message: "Login Failed",
+        error: error.toString(),
+      });   
+    }
+
 }
 
 
 const healthCheck = async (req, res) => {
+    console.log("Healthcheck hit!")
     return res.status(200).json({
                 healthcheckStatus : "Up and running"
     })
 }
 
-export { loginUser , registerUser, healthCheck};
+export { loginUser, registerUser, healthCheck }
--- fullstackSQL/index.js
+++ fullstackSQL/index.js
@@ -9,13 +9,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3000
 const app = express();
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser);
-
-app.use('/api/v1/users', UserRoute)
-
 app.use(cors({
     origin: process.env.BASE_URL,
     credentials: true,
@@ -23,10 +16,16 @@ app.use(cors({
 	allowedHeaders: ["Content-Type","Authorization"]
 }))
 
-app.get('/',(req,res) => {
-    res.send("UP!");
-    console.log('hit')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser);
+
+app.get('/', (req, res) => {
+    res.send('Working api call');
 })
+
+app.use('/api/v1/users/', UserRoute)
+
 
 app.listen(PORT, () => {
     console.log("listening on port : ", PORT);
